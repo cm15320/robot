c339311b0fdaa1eae34756891d51547ba682f28e
Chris Meehan <cm15320@bristol.ac.uk>
2016-08-07 22:13:38 +0100

    added starting experiment components, getting the correct rigid body data from the frame based on hash tables of key to name etc

--- robotTracking/Experiment.cs
+++ robotTracking/Experiment.cs
@@ -11,23 +11,77 @@ namespace robotTracking
     class Experiment
     {
         private RobotControl controller;
-        private RigidBodyData robotBase, robotTip;
+        //private RigidBodyData robotBase, robotTip;
+        private Hashtable htRigidBodiesNameToBody;
+        private double distanceBetween;
+        
+        
+        //private string[] bodyNames = new string[] { "robotBase", "robotTip" };
         // need to store the name of the rigid bodies mapped against their ID
-        private Hashtable rigidBodiesNameToID = new Hashtable();
+        private Hashtable rigidBodiesIDtoName = new Hashtable();
         // private List<RigidBody> blocks;
+
 
         public Experiment(RobotControl controller, List<RigidBody> mRigidBodies)
         {
+            htRigidBodiesNameToBody.Add("robotBase", new RigidBodyData());
+            htRigidBodiesNameToBody.Add("robotTip", new RigidBodyData());
             this.controller = controller;
             if(mRigidBodies.Count == 0)
             {
                 Console.WriteLine("Error, no rigid body info stored");
             }
+            foreach(RigidBody rb in mRigidBodies)
+            {
+                if(htRigidBodiesNameToBody.ContainsKey(rb.Name))
+                {
+                    int key = rb.ID.GetHashCode();
+                    rigidBodiesIDtoName.Add(key, rb.Name);
+                }
+            }
+            if(rigidBodiesIDtoName.Count == 0)
+            {
+                Console.WriteLine("Error, not added necessary rigid bodies to the hash table");
+            }
         }
 
         public void update(NatNetML.FrameOfMocapData currentFrame)
         {
-            
+            // get the correct rigid body data objects from the frame
+            foreach(RigidBodyData rb in currentFrame.RigidBodies)
+            {
+                int keyID = rb.ID.GetHashCode();
+                if(rigidBodiesIDtoName.ContainsKey(keyID))
+                {
+                    string name = (string)rigidBodiesIDtoName[keyID];
+                    htRigidBodiesNameToBody[name] = rb;
+                }
+            }
+
+            calculateDistanceBetween();
+        }
+
+        private void calculateDistanceBetween()
+        {
+            // If this doesn't work then get hash code from the string and use that instead
+            RigidBodyData robotBase = (RigidBodyData)htRigidBodiesNameToBody["robotBase"];
+            RigidBodyData robotTip = (RigidBodyData)htRigidBodiesNameToBody["robotTip"];
+
+            float x1 = robotBase.x;
+            float y1 = robotBase.y;
+            float z1 = robotBase.z;
+
+            float x2 = robotTip.x;
+            float y2 = robotTip.y;
+            float z2 = robotTip.z;
+
+            double distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+            distanceBetween = distance;
+        }
+
+        public double getDistanceBetween()
+        {
+            return distanceBetween;
         }
 
         public void calibrate()
